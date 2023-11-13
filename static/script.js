fd9c866a1c81c40f13d8ad9ee9bb5817b06fdbd6
oadelek01 <114633115+oadelek01@users.noreply.github.com>
2023-11-12 23:14:25 -0500

    Enhance Webpage Display and Add List Creation Functionality
    - Improved webpage display of search results
    - Modified filtering criteria for search results
    - Added functionality to create superhero name lists

--- static/script.js
+++ static/script.js
@@ -19,4 +19,43 @@ function searchSuperheroes() {
             headers: {
                 'Content-Type': 'application/json',
             },
-        })})}
+        })
+        .then(response => response.json())
+        .then(results => {
+            // Display the results on the webpage
+            const resultsList = document.getElementById('resultsList');
+            resultsList.innerHTML = '';
+            const filterResults = results.filter(hero => {
+                return hero.name.includes(searchCriteria) || hero.Race.includes(searchCriteria) || hero.Publisher.includes(searchCriteria);
+            })
+            if (hero_powers.length > 0) {
+                hero_powers.forEach(hero => {
+                    const listItem = document.createElement('li');
+                    listItem.appendChild(document.createTextNode(`${hero.hero_names}`))
+                    resultsList.appendChild(listItem);
+                });
+            }
+            else {
+                filterResults.forEach(hero => {
+                const listItem = document.createElement('li');
+                listItem.appendChild(document.createTextNode(`${hero.name} (${hero.Publisher}) - ${hero.Race}`))
+                resultsList.appendChild(listItem);
+            });
+            } 
+        })
+    })
+    .catch(error => console.error('Error:', error));  
+}
+
+function createList() {
+    const listName = document.getElementById('listName').value;
+    fetch(`http://localhost:3000/supernames`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "name": `${listName}`
+        }),
+    })
+}
